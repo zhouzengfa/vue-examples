d964328910a8c73a8ef6fe78bb5fd24cc7c5d02b
zhouzengfa <zengfa_zhou@163.com>
2020-08-05 23:57:39 +0800

    实现首页路由的重定向效果

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -31,7 +31,9 @@
             </el-submenu>
           </el-menu>
         </el-aside>
-        <el-main>Main</el-main>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
       </el-container>
     </el-container>
   </div>
