4134e5cdd2e49388569ad7aa0888e8747e7f4591
zhouzengfa <zengfa_zhou@163.com>
2020-08-05 19:17:56 +0800

    退出功能 删除token

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,18 @@
 <template>
     <div>
-      home
+      <el-button type="info" @click="quit">退出</el-button>
     </div>
 </template>
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  methods: {
+    quit () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
 }
 </script>
 
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -73,6 +73,10 @@
 </script>
 
 <style lang="less" scoped>
+  .login_container{
+    background-color: #2b4b6b;
+    height: 100%;
+  }
   .login_box {
     width: 450px;
     height: 300px;
