b580e9a77ef3bbfa1416eb966188edc0181c5e57
zhouzengfa <zengfa_zhou@163.com>
2020-08-05 18:47:59 +0800

    路由守卫控制页面访问权限

--- src/router/index.js
+++ src/router/index.js
@@ -26,4 +26,14 @@ const router = new VueRouter({
   routes
 })
 
+// 路由守卫规则
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') { return next() }
+
+  const token = window.sessionStorage.getItem('token')
+  if (!token) { return next('/login') }
+
+  next()
+})
+
 export default router
