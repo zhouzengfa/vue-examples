59a00ea3b4dddf097f63799047a00cd4f6675b62
zhouzengfa <zengfa_zhou@163.com>
2020-08-17 15:56:49 +0800

    tree-grid渲染操作

--- src/components/goods/Cat.vue
+++ src/components/goods/Cat.vue
@@ -7,6 +7,26 @@
         </template>
         <template v-slot:card>
           <el-button type="primary">添加分类</el-button>
+          <zk-table
+            border
+            :selection-type="false"
+            :expand-type="false"
+            show-index index-text="#"
+            :data="cateList" :columns="columns" role-key="cat_id">
+            <template slot="isok" slot-scope="scope">
+              <i class="el-icon-success" style="color: lightgreen" v-if="scope.row.cat_deleted == false"></i>
+              <i v-else class="el-icon-delete" style="color: red"></i>
+            </template>
+            <template slot="order" slot-scope="scope">
+              <el-tag v-if="scope.row.cat_level==0">一级</el-tag>
+              <el-tag v-else-if="scope.row.cat_level==1" type="success">二级</el-tag>
+              <el-tag v-else type="warning">三级</el-tag>
+            </template>
+            <template slot="operator" slot-scope="scope">
+              <el-button type="primary" icon="el-icon-edit" size="mini" @click="showEditDialog(scope.row)">编辑</el-button>
+              <el-button type="danger" icon="el-icon-delete" size="mini" @click="showDeleteDialog()">删除</el-button>
+            </template>
+          </zk-table>
         </template>
       </BaseCard>
     </div>
@@ -14,10 +34,12 @@
 
 <script>
 import BaseCard from '../common/BaseCard'
+import ZkTable from 'vue-table-with-tree-grid'
 
 export default {
   components: {
-    BaseCard
+    BaseCard,
+    ZkTable
   },
   name: 'Cat',
   data () {
@@ -28,7 +50,29 @@ export default {
         type: 3
       },
       cateList: [],
-      totalpage: 0
+      totalpage: 0,
+      columns: [
+        {
+          label: '分类名称',
+          prop: 'cat_name'
+        },
+        {
+          label: '是否有效',
+          type: 'template',
+          template: 'isok'
+        },
+        {
+          label: '排序',
+          type: 'template',
+          template: 'order'
+        },
+        {
+          label: '操作',
+          type: 'template',
+          template: 'operator',
+          width: 180
+        }
+      ]
     }
   },
   created () {
@@ -41,6 +85,12 @@ export default {
       if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
       this.cateList = res.data.result
       this.totalpage = res.data.total
+    },
+    showEditDialog (row) {
+      console.log('show edit dialog')
+    },
+    showDeleteDialog () {
+      console.log('show delete dialog')
     }
   }
 }
