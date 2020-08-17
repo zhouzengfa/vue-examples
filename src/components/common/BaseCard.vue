767bab3717e8cd1940dbb2700ed7f36e48a2e9d2
zhouzengfa <zengfa_zhou@163.com>
2020-08-17 14:13:18 +0800

    通用组件

---
+++ src/components/common/BaseCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <slot name="nav">
+        <el-breadcrumb-item><a href="/">权限管理</a></el-breadcrumb-item>
+        <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+      </slot>
+    </el-breadcrumb>
+    <el-card class="box-card">
+     <slot name = "card">default card</slot>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseCard'
+}
+</script>
+
+<style lang="less" scoped>
+  .el-breadcrumb {
+    font-size: 12px;
+    margin-bottom: 15px;
+  }
+  .box-card {
+    box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1) !important;
+  }
+  .el-table {
+    margin-top: 15px;
+    font-size: 12px;
+  }
+</style>
---
+++ src/components/goods/Cat.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <BaseCard>
+        <template v-slot:nav>
+          <el-breadcrumb-item><a href="/">商品管理</a></el-breadcrumb-item>
+          <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+        </template>
+        <template v-slot:card>
+          <el-button type="primary">添加分类</el-button>
+        </template>
+      </BaseCard>
+    </div>
+</template>
+
+<script>
+import BaseCard from '../common/BaseCard'
+
+export default {
+  components: {
+    BaseCard
+  },
+  name: 'Cat'
+}
+</script>
+
+<style lang="less" scoped>
+  .el-table {
+    margin-top: 15px;
+    font-size: 12px;
+  }
+</style>
