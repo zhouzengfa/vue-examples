5d839eab7a73df7844e36c8eec360ce7c869a022
zhouzengfa <zengfa_zhou@163.com>
2020-08-17 21:18:48 +0800

    商品参数组件

---
+++ src/components/goods/Params.vue
@@ -0,0 +1,20 @@
+<template>
+    <BaseCard>
+
+    </BaseCard>
+</template>
+
+<script>
+import BaseCard from '../common/BaseCard'
+
+export default {
+  name: 'Params',
+  components: {
+    BaseCard
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Users from '../components/user/Users'
 import Rights from '../components/power/Rights'
 import Roles from '../components/power/Roles'
 import Cat from '../components/goods/Cat'
+import Params from '../components/goods/Params'
 
 Vue.use(VueRouter)
 
@@ -22,6 +23,7 @@ const routes = [
       { path: '/users', component: Users },
       { path: '/rights', component: Rights },
       { path: '/roles', component: Roles },
+      { path: '/params', component: Params },
       { path: '/categories', component: Cat }
     ]
   }
