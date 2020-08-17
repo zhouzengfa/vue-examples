cf90192a804ae9d6a55130d89158dd935ce67219
zhouzengfa <zengfa_zhou@163.com>
2020-08-17 21:22:25 +0800

    获取商品分类列表数据

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -11,6 +11,22 @@ export default {
   name: 'Params',
   components: {
     BaseCard
+  },
+  data () {
+    return {
+      cateList: []
+    }
+  },
+  created () {
+    this.getCateList()
+  },
+  methods: {
+    async getCateList () {
+      const { data: res } = await this.$axios.get('categories', { params: { type: 3 } })
+      console.log('categories:', res)
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.catelist = res.data.result
+    }
   }
 }
 </script>
