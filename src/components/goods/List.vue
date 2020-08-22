170fc6b26e29809cf3d946eb413f4f579f6dfd13
zhouzengfa <zengfa_zhou@163.com>
2020-08-22 22:30:57 +0800

    时间转换

--- src/components/goods/List.vue
+++ src/components/goods/List.vue
@@ -20,7 +20,11 @@
         <el-table-column label="商品名称" prop="goods_name"> </el-table-column>
         <el-table-column label="商品价格(元)" prop="goods_price"> </el-table-column>
         <el-table-column label="商品重量" prop="goods_weight"> </el-table-column>
-        <el-table-column label="创建时间" prop="add_time"> </el-table-column>
+        <el-table-column label="创建时间" width="140px">
+          <template v-slot:="scope">
+            {{scope.row.add_time | dateFormat}}
+          </template>
+        </el-table-column>
         <el-table-column label="操作" width="180px">
           <template v-slot:="scope">
             <el-button type="primary" size="mini" icon="el-icon-edit" >编辑</el-button>
@@ -28,6 +32,15 @@
           </template>
         </el-table-column>
       </el-table>
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[1, 2, 5, 10]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="totalpage">
+      </el-pagination>
     </template>
       商品列表
   </base-card>
@@ -58,9 +71,19 @@ export default {
       const { data: res } = await this.$axios.get('goods', { params: this.queryInfo })
       console.log('goodlist', res)
       if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
-      this.totalpage = res.total
+      this.totalpage = res.data.total
       this.goodList = res.data.goods
       console.log('goodlist:', this.goodList)
+    },
+    handleSizeChange (pagesize) {
+      console.log('page size change:', pagesize)
+      this.queryInfo.pagesize = pagesize
+      this.getGoodList()
+    },
+    handleCurrentChange (pagenum) {
+      console.log('handle current change. page num:', pagenum)
+      this.queryInfo.pagenum = pagenum
+      this.getGoodList()
     }
   },
   created () {
@@ -71,7 +94,7 @@ export default {
 
 <style lang="less" scoped>
 .el-table {
-  margin-top: 15px;
+  margin: 15px 0px;
   font-size: 12px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -66,6 +66,18 @@ Vue.prototype.$axios = axios
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
 
+Vue.filter('dateFormat', function (date) {
+  const dt = new Date(date * 1000)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDay() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 new Vue({
   router,
   render: h => h(App)
