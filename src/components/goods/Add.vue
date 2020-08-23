6157e385fade471f3e50fed8f4dd1482f4279fff
zhouzengfa <zengfa_zhou@163.com>
2020-08-23 09:11:58 +0800

    商品列表-渲染商品添加的基本页面结构

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -1,12 +1,23 @@
 <template>
-  <div>
-    商品添加
-  </div>
+  <base-card>
+    <template v-slot:nav>
+      <el-breadcrumb-item><a href="/">商品管理</a></el-breadcrumb-item>
+      <el-breadcrumb-item>商品添加</el-breadcrumb-item>
+    </template>
+    <template v-slot:card>
+      <el-alert title="添加商品信息" type="info" center show-icon :closable="false"> </el-alert>
+    </template>
+  </base-card>
 </template>
 
 <script>
+import BaseCard from '../common/BaseCard'
+
 export default {
-  name: 'Add'
+  name: 'Add',
+  components: {
+    BaseCard
+  }
 }
 </script>
 
