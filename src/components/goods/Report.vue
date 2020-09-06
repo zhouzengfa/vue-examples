5e6aeb10b815cdb518c80ed61d8c48f9173cd553
zhouzengfa <zengfa_zhou@163.com>
2020-09-06 18:11:13 +0800

    数据统计-通过路由加载数据报表组件

---
+++ src/components/goods/Report.vue
@@ -0,0 +1,26 @@
+<template>
+  <base-card>
+    <template v-slot:nav>
+      <el-breadcrumb-item><a href="/">数据统计</a></el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </template>
+    <template v-slot:card>
+
+    </template>
+  </base-card>
+</template>
+
+<script>
+import BaseCard from '../common/BaseCard'
+
+export default {
+  name: 'Report',
+  components: {
+    BaseCard
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from '../components/goods/Params'
 import List from '../components/goods/List'
 import Add from '../components/goods/Add'
 import Orders from '../components/goods/Oreders'
+import Report from '../components/goods/Report'
 
 Vue.use(VueRouter)
 
@@ -30,6 +31,7 @@ const routes = [
       { path: '/goods', component: List },
       { path: '/add', component: Add },
       { path: '/categories', component: Cat },
+      { path: '/reports', component: Report },
       { path: '/orders', component: Orders }
     ]
   }
