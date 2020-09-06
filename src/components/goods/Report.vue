96815d028555c215891145b242de710c14a7cf44
zhouzengfa <zengfa_zhou@163.com>
2020-09-06 18:48:59 +0800

    数据统计-安装Echarts并渲染Demo图表

--- src/components/goods/Report.vue
+++ src/components/goods/Report.vue
@@ -5,18 +5,46 @@
       <el-breadcrumb-item>数据报表</el-breadcrumb-item>
     </template>
     <template v-slot:card>
-
+      <div id="main" style="width: 600px;height:400px;"></div>
     </template>
   </base-card>
 </template>
 
 <script>
 import BaseCard from '../common/BaseCard'
+import echarts from 'echarts'
 
 export default {
   name: 'Report',
   components: {
     BaseCard
+  },
+  mounted () {
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('main'))
+
+    // 指定图表的配置项和数据
+    var option = {
+      title: {
+        text: 'ECharts 入门示例'
+      },
+      tooltip: {},
+      legend: {
+        data: ['销量']
+      },
+      xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+      },
+      yAxis: {},
+      series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }]
+    }
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option)
   }
 }
 </script>
