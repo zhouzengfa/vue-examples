026e5b6bd4a14081eb694c07751ff01468256c5f
zhouzengfa <zengfa_zhou@163.com>
2020-09-06 18:58:51 +0800

    数据统计-获取折线图数据并渲染图表

--- src/components/goods/Report.vue
+++ src/components/goods/Report.vue
@@ -13,38 +13,76 @@
 <script>
 import BaseCard from '../common/BaseCard'
 import echarts from 'echarts'
+import _ from 'lodash'
 
 export default {
   name: 'Report',
+  data () {
+    return {
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+    }
+  },
   components: {
     BaseCard
   },
-  mounted () {
+  async mounted () {
     // 基于准备好的dom，初始化echarts实例
     var myChart = echarts.init(document.getElementById('main'))
+    const { data: res } = await this.$axios.get('reports/type/1')
+    if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+    var options = _.merge(this.options, res.data)
 
     // 指定图表的配置项和数据
-    var option = {
-      title: {
-        text: 'ECharts 入门示例'
-      },
-      tooltip: {},
-      legend: {
-        data: ['销量']
-      },
-      xAxis: {
-        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-      },
-      yAxis: {},
-      series: [{
-        name: '销量',
-        type: 'bar',
-        data: [5, 20, 36, 10, 10, 20]
-      }]
-    }
+    // var option = {
+    //   title: {
+    //     text: 'ECharts 入门示例'
+    //   },
+    //   tooltip: {},
+    //   legend: {
+    //     data: ['销量']
+    //   },
+    //   xAxis: {
+    //     data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+    //   },
+    //   yAxis: {},
+    //   series: [{
+    //     name: '销量',
+    //     type: 'bar',
+    //     data: [5, 20, 36, 10, 10, 20]
+    //   }]
+    // }
 
     // 使用刚指定的配置项和数据显示图表。
-    myChart.setOption(option)
+    myChart.setOption(options)
   }
 }
 </script>
